f22d7c2d962a6721373646f60207763054b926d6
kaeuloze <mariskaesapurnomo@gmail.com>
2026-03-13 16:05:30 +0700

    inisialisasi project Spring Boot dan konfigurasi main class aplikasi.

---
+++ src/main/java/com/tugas/ktp/TugasCrud20230140167Application.java
@@ -0,0 +1,13 @@
+package com.tugas.ktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140167Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140167Application.class, args);
+	}
+
+}

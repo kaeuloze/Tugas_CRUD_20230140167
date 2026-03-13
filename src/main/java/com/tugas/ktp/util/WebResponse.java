7442936b1d66773162e39475a8d3e8b875a252dc
kaeuloze <mariskaesapurnomo@gmail.com>
2026-03-13 16:01:18 +0700

    Menambahkan class WebResponse sebagai wrapper standar untuk respon API.

---
+++ src/main/java/com/tugas/ktp/util/WebResponse.java
@@ -0,0 +1,16 @@
+package com.tugas.ktp.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+    private String message;
+    private String status;
+    private T data;
+}

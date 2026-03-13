7d139b5a13f1f496c702cf66c9d8dbb179806cc5
kaeuloze <mariskaesapurnomo@gmail.com>
2026-03-13 16:06:08 +0700

    tambah pengujian dasar contextLoads untuk memastikan aplikasi dapat berjalan

---
+++ src/test/java/com/tugas/ktp/TugasCrud20230140167ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140167ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
